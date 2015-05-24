507a34f0c3b563a96a9c2b007253ad178c4534d3
hiroshi kobayashi <kobayashi@ans-web.co.jp>
2015-05-24 14:12:43 +0900

    秒で更新する時計

--- src/Tutorial1.c
+++ src/Tutorial1.c
@@ -3,6 +3,28 @@
 static Window *window;
 static TextLayer *text_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00:00"), "%H:%M:%S", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00:00"), "%I:%M:%S", tick_time);
+  }
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "time: %s", buffer);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(text_layer, buffer);
+}
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
 }
@@ -25,8 +47,14 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button!!!");
+  text_layer = text_layer_create((GRect) {
+    .origin = { 0, 51 },
+    .size = { bounds.size.w, 72 }
+  });
+
+  text_layer_set_text(text_layer, "00:00:00");
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
@@ -44,16 +72,25 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(window, animated);
+  update_time();
 }
 
 static void deinit(void) {
   window_destroy(window);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+
 int main(void) {
   init();
 
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  /* tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); */
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 
   app_event_loop();
   deinit();
